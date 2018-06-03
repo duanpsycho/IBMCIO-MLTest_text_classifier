f2ac5d5932d2e2268c2b5ed94de60506bf9b5728
Duan Nunes Alves da Silva <39650422+duanpsycho@users.noreply.github.com>
2018-06-03 20:53:52 -0300

    Upload

---
+++ classifier.py
@@ -0,0 +1,57 @@
+import os
+
+entrada = "textos"
+tipo_teste, texto_teste = [], []
+tipo_treino, texto_treino = [], []
+
+# Leitura dos arquivos
+for pastas in os.listdir(entrada):
+    diretorio = os.listdir(entrada+"\{0}".format(pastas))
+    for arquivos in diretorio:
+        dir = str(entrada + '\\' + pastas + '\\' + arquivos)
+        file = open(str(dir), 'r', encoding='utf-8').read()
+        if pastas == 'testes':
+            texto_teste.append(file)
+            if "carta" in arquivos:
+                tipo_teste.append("carta_de_amor")
+            elif "receita" in arquivos:
+                tipo_teste.append("receita_de_bolo")
+        else:
+            texto_treino.append(file)
+            if "carta" in arquivos:
+                tipo_treino.append("carta_de_amor")
+            elif "receita" in arquivos:
+                tipo_treino.append("receita_de_bolo")
+
+# Prepara Treino
+import pandas as pd
+df_treino = pd.DataFrame()
+df_treino['tipo'] = tipo_treino
+df_treino['texto'] = texto_treino
+
+# Vetorização de palavras
+from sklearn.feature_extraction.text import CountVectorizer
+vetor_modelo = CountVectorizer()
+vetor_modelo.fit(df_treino['texto'])
+vetor_treino = vetor_modelo.transform(df_treino['texto'])
+
+# Importa Naive Bayes
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(vetor_treino, df_treino['tipo'])
+
+# Prepara teste
+df_teste = pd.DataFrame()
+df_teste['texto'] = texto_teste
+df_teste['tipo'] = tipo_teste
+
+# Transforma teste em vetor
+vetor_teste = vetor_modelo.transform(df_teste['texto'])
+
+# Predicao dos testes
+predicao = modelo.predict(vetor_teste)
+
+# Avaliacao de resultado
+from sklearn import metrics
+avalicao = metrics.accuracy_score(predicao, df_teste['tipo'])
+print("Resultado da predição: {0}%".format(int(avalicao*100)))
